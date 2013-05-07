e39bf02c4e5934f28b915567cdb83f0aa9290d92
Brandon Whaley <redkrieg@gmail.com>
2013-05-07 16:55:01 -0400

    playing around with forms some more

--- sadbin.py
+++ sadbin.py
@@ -2,21 +2,21 @@
 
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.wtf import Form
-from wtforms import TextField
+from wtforms import TextAreaField
 import flask
 
 app = flask.Flask(__name__)
 app.config.from_pyfile('instance/application.cfg')
 
 class Paste(Form):
-    content = TextField('Content')
+    paste_content = TextAreaField('Paste Data:')
 
 @app.route('/', methods=('GET', 'POST'))
 def main():
     form = Paste()
     if form.validate_on_submit():
         return flask.render_template('base.html', form=form,
-                                     message=form.content.data)
+                                     message=form.paste_content.data)
     return flask.render_template('base.html', form=form, message="Nothing")
 
 Bootstrap(app)
