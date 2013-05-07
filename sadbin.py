80f5fb98cd2170233fb9fd170e56aa5b6867f51a
Brandon Whaley <redkrieg@gmail.com>
2013-05-06 20:34:12 -0400

    just testing

---
+++ sadbin.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import TextField
+import flask
+
+app = flask.Flask(__name__)
+app.config.from_pyfile('instance/application.cfg')
+
+class Paste(Form):
+    content = TextField('Content')
+
+@app.route('/', methods=('GET', 'POST'))
+def main():
+    form = Paste()
+    if form.validate_on_submit():
+        return flask.render_template('base.html', form=form,
+                                     message=form.content.data)
+    return flask.render_template('base.html', form=form, message="Nothing")
+
+Bootstrap(app)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
